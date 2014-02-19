d1834040bd2aa748927f2fa775486e76547e79e7
Luis Doubrava <luis@cg.nl>
2014-02-19 09:59:26 +0100

    change default min_date setting to beginning of month

--- lib/spree_advanced_reporting.rb
+++ lib/spree_advanced_reporting.rb
@@ -6,6 +6,6 @@ module SpreeAdvancedReporting
   mattr_accessor :default_min_date
 
   def self.default_min_date
-    @default_min_date ||= Time.now - 30.days
+    @default_min_date ||= Time.now.beginning_of_month
   end
 end
